f45a6c1eba06dfc425966d333f10844d9d5f08af
imalexis <alexis9007s@gmail.com>
2021-02-09 17:48:53 +0800

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,20 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by alexis on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
